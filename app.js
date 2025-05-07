085b4068be966704f5f6a13f026b9103d3febaff
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-05-07 14:29:13 -0300

    214

---
+++ app.js
@@ -0,0 +1,16 @@
+const swither = document.querySelector('.botao');
+
+swither.addEventListener('click',function(){
+    document.body.classList.toggle('tema-escuro');
+    document.body.classList.toggle('temaclaro');
+
+
+    var nomedaclasse = document.body.className;
+
+    if (nomedaclasse.includes("tema-claro")){
+        this.textcontent = "escuro";
+    } else{
+        this.textcontent = "claro";
+    }
+
+});
